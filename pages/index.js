59f540e5b4d31bfe28ee0c970bd39f118da7ce4b
Ian Matthew Sobule <imsobule@gmail.com>
2021-02-07 14:51:40 -0600

    move some code around

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,62 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import React from "react";
+import { useDebounce } from "../utils/hooks";
+import * as NextLink from "next/link";
 export default function Home() {
+  // Track port
   const [port, setPort] = React.useState(null);
-  const debouncedPortVal = useDebounce(port, 500);
-  const [command, setCommand] = React.useState(formatCopyCommand(port));
-  const commandRef = React.useRef(null);
-  const portRef = React.useRef(null);
+  // Debounce the value, so format hook doesn't run every change
+  const debouncedPortVal = useDebounce(port, 150);
+  // generated command
+  const command = React.useMemo(() => formatCopyCommand(debouncedPortVal), [
+    debouncedPortVal,
+  ]);
+  // ref to the hidden command input (browser can only copy from a "selected" input)
+  const commandCopyRef = React.useRef(null);
+  // port ref used for triggiring the copy function when input has focus
+  const portInputRef = React.useRef(null);
+  // has the user copied the code? used to render "copy" vs "copied" in UI
   const [copied, setCopied] = React.useState(false);
-  const copyOnChange = (e) => {
+
+  //Change handler for for port input
+  function copyOnChange(e) {
     e.preventDefault();
     setCopied(false);
     setPort(e.target.value);
-  };
+  }
+  // function to copy the command from hidden input to the clipboard
   function copyCode() {
     setCopied(true);
-    commandRef.current.select();
+    commandCopyRef.current.select();
     document.execCommand("copy");
   }
+  // watch the inpur ref -- if enter is pressed while focus, trigger copy
   function inputPress(e) {
-    if(portRef.current === document.activeElement && e.charCode === 13){
-      copyCode()
+    if (portInputRef.current === document.activeElement && e.charCode === 13) {
+      copyCode();
     }
   }
-  React.useEffect(() => {
-    setCommand(formatCopyCommand(port));
-  }, [port]);
 
+  // By default focus on the portInput on load
   React.useEffect(() => {
-    if (portRef.current) {
-      portRef.current.focus();
+    if (portInputRef.current) {
+      portInputRef.current.focus();
     }
-  }, [portRef.current]);
-  return (
-    <div 
-    className="min-h-screen h-full flex flex-col items-center justify-center relative">
-      {/* <div className="absolute w-full h-full bg-gray-800 bg-opacity-20"></div> */}
+  }, [portInputRef.current]);
 
+  return (
+    <div className="min-h-screen h-full flex flex-col items-center justify-center relative mx-5">
+      <div className="flex flex-row w-full justify-end items-center max-w-screen-xl mt-5">
+        <div className="flex flex-row items-center space-x-3 ">
+          <div className="font-bold cursor-pointer">home</div>
+          <div className="cursor-pointer">bash script</div>
+          <div className="cursor-pointer">about</div>
+          <div className="cursor-not-allowed  text-black text-opacity-50">
+            windows <span className="text-xs">(coming soon...)</span>
+          </div>
+        </div>
+      </div>
       <div className="m-auto h-full w-full max-w-prose flex flex-col items-center justify-center space-y-3">
         <div className="flex flex-col font-mono w-full relative">
           <h1 className="text-4xl font-extralight mb-4 font-serif">
@@ -46,7 +66,7 @@ export default function Home() {
           <input
             className="absolute opacity-0 pointer-events-none -left-full"
             value={command}
-            ref={commandRef}
+            ref={commandCopyRef}
             readOnly
             type="text"
           />
@@ -56,7 +76,7 @@ export default function Home() {
             autoFocus={true}
             onChange={copyOnChange}
             onKeyPress={inputPress}
-            ref={portRef}
+            ref={portInputRef}
             value={port}
             className="text-lg flex-1 p-2 border-black focus:outline-none focus:ring focus:ring-green-200 border-solid border-2"
             placeholder="port number"
@@ -67,10 +87,7 @@ export default function Home() {
           <div>1.) copy command below </div>
           <div>2.) paste in terminal</div>
         </div>
-        <code
-          onClick={copyCode}
-          className="bg-gray-800 hover:bg-gray w-full cursor-pointer p-2 text-white text-sm flex flex-row items-center justify-between"
-        >
+        <code className="bg-gray-800 hover:bg-gray w-full  p-2 text-white text-sm flex flex-row items-center justify-between">
           <div>
             <span className="select-none">~ </span>
             {command}
@@ -107,27 +124,15 @@ function formatCopyCommand(port) {
   return `lsof -ti :${port || "{PORT}"} | xargs kill`;
 }
 
-// Credit to ... https://usehooks.com/useDebounce/
-function useDebounce(value, delay) {
-  // State and setters for debounced value
-  const [debouncedValue, setDebouncedValue] = React.useState(value);
-
-  React.useEffect(
-    () => {
-      // Update debounced value after delay
-      const handler = setTimeout(() => {
-        setDebouncedValue(value);
-      }, delay);
-
-      // Cancel the timeout if value changes (also on delay change or unmount)
-      // This is how we prevent debounced value from updating if value is changed ...
-      // .. within the delay period. Timeout gets cleared and restarted.
-      return () => {
-        clearTimeout(handler);
-      };
-    },
-    [value, delay] // Only re-call effect if value or delay changes
+const Link = (props) => {
+  const { disabled } = props;
+  return (
+    <NextLink {...props}>
+      <a
+        className={`${
+          disabled ? "cursor-not-allowed font-normal" : "cursor-pointer"
+        } font-`}
+      ></a>
+    </NextLink>
   );
-
-  return debouncedValue;
-}
+};
